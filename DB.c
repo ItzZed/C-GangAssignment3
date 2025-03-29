29d9a8adb50bd9125a7115e954262927e26fc47a
tomschu747 <schustert@mymacewan.ca>
2025-03-29 04:34:39 +0000

    Added incomplete freeDB()

--- DB.c
+++ DB.c
@@ -10,3 +10,55 @@
 
 #include "DB.h"       /* Import the public database header. */
 #include "DB_impl.h"  /* Import the private database header */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+DataBase *Db = NULL;
+
+void freeDB()
+{
+    if (Db != NULL)
+    {
+        // Free each entry in the picnicTable.
+        if (Db->picnicTableTable != NULL)
+        {
+            for (int i = 0; i < Db->picnicTableCount; i++)
+            {
+                // Free the picnicTable entry and any members with dynamic memory.
+                if (Db->picnicTableTable[i] != NULL)
+                {
+                    // free(Db->picnicTableTable[i]->streetAvenue);
+                    free(Db->picnicTableTable[i]);
+                }
+            }
+            free(Db->picnicTableTable);
+            Db->picnicTableTable = NULL; // Set to NULL after freeing
+        }
+        // Free all of the tables if not NULL.
+        if (Db->tableTypeTable != NULL)
+        {
+            for (int i = 0; i < Db->tableTypeTable->size; i++)
+            {
+                if (Db->tableTypeTable->entries[i] != NULL)
+                {
+                    free(Db->tableTypeTable->entries[i]);
+                }
+            }
+            free(Db->tableTypeTable->entries);
+            free(Db->tableTypeTable);
+            Db->tableTypeTable = NULL; // Set to NULL after freeing
+        }
+            
+        // if (Db->surfaceMaterialTable != NULL)
+        //     free(Db->surfaceMaterialTable);
+        // if (Db->structuralMaterialTable != NULL)
+        //     free(Db->structuralMaterialTable);
+        // if (Db->neighborhoodTable != NULL)
+        //     free(Db->neighborhoodTable);
+
+        // Free the DataBase structure itself.
+        free(Db);
+        Db = NULL;
+    }
+}
